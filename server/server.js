4e3a6b96a8f9ecfa4d998a289562b4a99a2bbd57
rancheng <rancheng@scrmtech.com>
2022-09-23 17:25:15 +0800

    1

--- server/server.js
+++ server/server.js
@@ -1,24 +1,42 @@
 const Koa = require('koa');
 const app = new Koa();
+var fs = require('fs')
+var path = require('path')
 
 const Router = require('koa-router');
 var router = new Router();
-var fs = require('fs')
-var path = require('path')
+
+const serve = require('koa-static');
+app.use(serve(
+    path.resolve(__dirname,'../client'))
+);
+
+
 app.use(router.routes());
 app.use(router.allowedMethods());
+
+
+
+
+
+
+// 通过jsonp 方式换取语言包
 router.get('/requestLanguagePack.js', async (ctx) => {
-
     var languageType = ctx.request.url.split('?')[1].split(('='))[1]
-
-
     var res = fs.readFileSync(
         path.resolve(__dirname,'./languages/'+ languageType +'/data.json'),
         'utf-8'
     )
     console.log(res.toString())
-
     ctx.body = `window.languagePack = ${res}`;
+})
+
+
+
+router.get('/api/description', async (ctx) => {
+
+
+    ctx.body = `你好， 这是后端返回的数据`;
 })
 
 
